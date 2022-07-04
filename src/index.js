de198384380ca834dfc5ef5e60e8a7c3d4713a2a
theandersonn <theandersonn@gmail.com>
2022-07-04 16:48:34 -0300

    criar conexão com o banco de dados

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./db/connect');
 const cors = require('cors');
 const menuRoutes = require('./routes/menuRoutes');
 
@@ -9,4 +10,13 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/menu', menuRoutes);
 
-app.listen(3333);
+const startApp = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(process.env.PORT, console.log(`Servidor funcionando em http://localhost:${process.env.PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
