13c7a557255e633a69c54ef9a8e230cec13a6b42
theandersonn <theandersonn@gmail.com>
2022-07-04 16:58:32 -0300

    implementar método criar menu

--- src/controllers/menus.js
+++ src/controllers/menus.js
@@ -1,6 +1,9 @@
+const Menu = require('../models/menus');
+
 const createMenu = async (req, res) => {
   try {
-    res.status(201).json({ message: 'Ok!' });
+    const menu = await Menu.create(req.body);
+    res.status(201).json({ menu, message: 'Menu criado com sucesso.' });
   } catch (error) {
     res.status(500).json({ error });
   }
